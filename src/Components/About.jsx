0b1f0bc4a2525c465e930d8cb52d8d9dd7e309e5
ROKHITHRAN <118052257+ROKHITHRAN@users.noreply.github.com>
2023-11-25 15:09:53 +0530

    Update About.jsx
    I changed About Page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
             </div>
             <div className='abtcontainercontent'>
                 <p style={{color:'black'}}>
-                Taking advantage of vast warehouses across the India, we stock over 1 million titles for immediate delivery -- that's more titles than any other online bookseller.
+                Taking advantage of vast warehouses across the India and other parts of world, we stock over 1 million titles for immediate delivery -- that's more titles than any other online bookseller.
 <br></br>
 <br></br>
 With so many titles, it is vital to give customers an easy way to find precisely the books they are looking for. Our search engine enables customers to locate books by title, author, or keyword in a few seconds at most. Customers with a general idea of what they want can use our Browse pages to sift through hundreds of categories to find exactly the right book. To further assist users, we offer descriptions and reviews, and our See Inside program lets customers read excerpts from tens of thousands of titles. We also offer editor recommendations and customer reviews on hundreds of thousands of titles.
